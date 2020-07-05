2be35462148182017fd71422c177d57faf5c9379
LuisLeopardi <supernano1998@gmail.com>
2020-07-05 14:31:34 -0400

    commit

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ if(process.env.NODE_ENV === 'production') {
   })
 }
 
-const server = require('http').createServer(app);
+const server = require('https').createServer(app);
 const port = process.env.PORT || 5000;
 server.listen(port);
 const io = require('socket.io')(server);
